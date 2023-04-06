67ed71dd3412fd67949f5b66f14f920244ddc0fe
Mylo Cyrus <irahiggins740@gmail.com>
2023-04-06 10:32:46 -0700

    Add files via upload

---
+++ origin-protocol.jl
@@ -0,0 +1,7 @@
+helm upgrade $(cat .namespace) devops/kubernetes/charts/origin \
+          -f devops/kubernetes/charts/origin/values.yaml \
+          -f devops/kubernetes/values/origin/values-$(cat .namespace).yaml \
+          -f devops/kubernetes/values/origin/secrets-$(cat .namespace).yaml
+timeout: '3600s'
+options:
+  machineType: 'N1_HIGHCPU_32'
